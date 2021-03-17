5738945f795781e401125867b7d520b31656cb96
Christos Cunning <christoscunn@gmail.com>
2021-03-16 20:28:26 -0700

    updated local files from S3 bucket live files. (for some reason dev env was behind production env)

--- script-v17.js
+++ script-v17.js
@@ -1,20 +1,20 @@
 
 $(document).ready(function() {
-	$("#body").load("page1-v3.html");
+	$("#body").load("page1-v5.html");
 	$("#page1text").css("color","#a3c6cf");
 	$("#page1").click(function() {
 		$("#page1text").css("color","#a3c6cf");
 		$("#page2text").css("color","white");
 		$("#page3text").css("color","white");
 		$("#page4text").css("color","white");
-		$("#body").load("page1-v3.html");
+		$("#body").load("page1-v5.html");
 	});
 	$("#page2").click(function() {
 		$("#page1text").css("color","white");
 		$("#page2text").css("color","#a3c6cf");
 		$("#page3text").css("color","white");
 		$("#page4text").css("color","white");
-		$("#body").load("page2-v3.html");
+		$("#body").load("page2-v9.html");
 	});
 	$("#page3").click(function() {
 		$("#page1text").css("color","white");
