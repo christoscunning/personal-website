9adff8a69451adacb9cf6c0998206ae730fc4a78
Christos Cunning <christoscunn@gmail.com>
2021-03-16 21:31:23 -0700

    update resume and file names to push to production (S3)

--- script-v18.js
+++ script-v18.js
@@ -1,27 +1,27 @@
 
 $(document).ready(function() {
-	$("#body").load("page1-v5.html");
+	$("#body").load("page1-v6.html");
 	$("#page1text").css("color","#a3c6cf");
 	$("#page1").click(function() {
 		$("#page1text").css("color","#a3c6cf");
 		$("#page2text").css("color","white");
 		$("#page3text").css("color","white");
 		$("#page4text").css("color","white");
-		$("#body").load("page1-v5.html");
+		$("#body").load("page1-v6.html");
 	});
 	$("#page2").click(function() {
 		$("#page1text").css("color","white");
 		$("#page2text").css("color","#a3c6cf");
 		$("#page3text").css("color","white");
 		$("#page4text").css("color","white");
-		$("#body").load("page2-v9.html");
+		$("#body").load("page2-v11.htm");
 	});
 	$("#page3").click(function() {
 		$("#page1text").css("color","white");
 		$("#page2text").css("color","white");
 		$("#page3text").css("color","#a3c6cf");
 		$("#page4text").css("color","white");
-		$("#body").load("page3-v2.html");
+		$("#body").load("page3-v3.html");
 	});
 	$("#page4").click(function() {
 		$("#page1text").css("color","white");
